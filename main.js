c59f99937ec5558f0dfb751cfb604bfce623387d
Bogdan Kolesov <bogdan__kolesov@bk.ru>
2021-01-10 21:35:54 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+//Объявление переменной
+
+//let number = 5;
+///const leftBorderWidth = 1; //Прямых констант в JS не бывает
+
+
+
+//Объект
+/*const obj = {
+    name: 'John',
+    Age: 25,
+    isMarried: false
+};
+console.log(obj.name);
+*/
+//Массив
+/*let images = ['plum.png', 'tree.png', 'stone.png'];
+console.log(images[0]);
+*/
+//РАБОТА С ПОЛЬЗОВАТЕЛЕМ
+
+//alert('Hello, world!')
+
+//const result = confirm('Да или нет?');
+
+//const answer = prompt('Вам есть 18 лет?', '18');
+//Вся информация от пользователя приходит в виде строк
+
+//Информация в виде числа
+//const answer = +prompt('Вам есть 18 лет?', '18');
+
+//Добавление, напрмер, вопросов в массив
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваша фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '18');
+
+//ИНТЕРПОЛЯЦИЯ
+
+//const category = 'toys';
+//console.log(`https://some.com/${category}/3`); //БЭКТИКИ
