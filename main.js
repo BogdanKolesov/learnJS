b4ba5ae6031b83d5aa1e4391a8de3bde0220580c
Bogdan Kolesov <bogdan__kolesov@bk.ru>
2021-01-10 23:15:10 +0300

    После урока "Условия"

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ console.log(images[0]);
 //ПРАКТИКА №1!!!!!!!!!!!!!!!!!!!
 
 
-const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '1');
+/*const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '1');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -65,4 +65,64 @@ const d = prompt('Оценка фильма', '');
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB.movies);
+console.log(personalMovieDB.movies);*/
+
+//УСЛОВИЯ
+/*
+if (4 == 9) {
+    console.log("Бывает");
+} else {
+    console.log("Nooooooooo!");
+}
+
+let num = 15;
+(num > 25) ? console.log('Ok'): console.log('No?'); //Тернарный оператор
+*/
+/*
+let num = 15;
+switch (num) { //Конструкция switch всегда идет на СТРОГОЕ сравнение
+    case 49:
+        console.log('Неверно!');
+        break;
+    case 15:
+        console.log('Верно!');
+        break;
+    default: //Если ни одно значение не верно
+        console.log('Не в этот раз');
+        break;
+}
+
+*/
+
+//ЦИКЛЫ
+
+//let num = 50;
+/*
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+*/
+
+
+/*
+do {
+    console.log(num);
+    num++;
+}
+
+while (num <= 55);
+*/
+/*
+for (let i = 1; i < 8; i++) {
+    if (i === 3) {
+        break; //Закончить
+    }
+    if (i === 7) {
+        continue; //Пропустить
+    }
+    console.log(i);
+    console.log(num);
+    num++;
+}
+*/
