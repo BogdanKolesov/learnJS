dbc4d1a8ac0ec32f54434099409dafe8052e1d0e
Bogdan Kolesov <bogdan__kolesov@bk.ru>
2021-01-10 23:52:10 +0300

    Практика #2

--- main.js
+++ main.js
@@ -126,3 +126,48 @@ for (let i = 1; i < 8; i++) {
     num++;
 }
 */
+
+//ПРАКТИКА №2!!!!!!!!!!!!!!!!!!!
+
+/*
+const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '1');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
+
+
+for (let i = 0; i < 2; i++) {
+    if (i === 2) {
+        break; //Закончить
+    }
+    const a = prompt('Один из фильмов?', ''),
+        b = prompt('Оценка фильма', '');
+
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+
+}
+console.log(personalMovieDB.movies);
+
+if (personalMovieDB.count < 10) {
+    console.log('Много фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы - классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы - киноман!');
+} else {
+    console.log('Произошла ошибка');
+}
+*/
