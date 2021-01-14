82341db8a35eb157517a060bdb3ad5fa012f9fcf
Bogdan Kolesov <bogdan__kolesov@bk.ru>
2021-01-14 18:30:04 +0300

    Параметры документа

--- Параметры документа, окна и работа с ними/script.js
+++ Параметры документа, окна и работа с ними/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const box = document.querySelector('.box'),
+    btn = document.querySelector('button');
+
+const width = box.clientWidth;
+const height = box.clientHeight;
+
+btn.addEventListener('click', () => {
+    // box.style.height = box.scrollHeight + 'px';
+    console.log(box.scrollTop);
+});
+
+console.log(box.getBoundingClientRect().top);
+
+const style = window.getComputedStyle(box); //через запятую  можно записать стиль псевдоэлемента
+console.log(style.display);
